a0b6873e7239aa4b6c147135ad7e9dea9808e367
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-01-03 16:46:47 +0100

    Refacto clock component from class to function

--- src/Clock.js
+++ src/Clock.js
@@ -1,37 +1,70 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
-class Clock extends Component{
+let timerId;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-        };
-    };
+function Clock() {
 
-    componentDidMount() {
-        this.timerId = setInterval(() => {
-                this.setState((state, props) => {
-                    return {
-                        date: new Date(),
-                    };
-                });
-            }, 1000);
-    };
+    const [clock, setClock] = useState(new Date());
 
-    componentDidUpdate() {
-        
-    };
+    //S'execute à chaque fois que le composant est monté.
+    useEffect(() => {
+        //Le setInterval permet de avec setClock de rafraichir toute les secondes 1000 le state du composant clock
+        timerId = setInterval(() => {
+            setClock(new Date());
+        }, 1000);
 
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-    };
+        //Dans le useEffect il est possible d'utiliser un return qui renvoie une fonction anonyme.
+        //Dans le cas présent on cherche à démonter le composant et à se désabonner du setInterval.
+        return () => {
+            clearInterval(timerId);
+        }
+    }, []); //Tableau des dépendances qui permet d'observer le changement de valeurs dans le state
 
-    render(){
-        return (
-            <p>It is {this.state.date.toLocaleTimeString()}</p>
-        );
-    };
-};
+    return (
+        <p>It is {clock.toLocaleTimeString()}</p>
+    );
+
+}
 
 export default Clock;
+
+
+
+
+// import { Component } from "react";
+
+// class Clock extends Component{
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             date: new Date(),
+//         };
+//     };
+
+//     componentDidMount() {
+//         this.timerId = setInterval(() => {
+//                 this.setState((state, props) => {
+//                     return {
+//                         date: new Date(),
+//                     };
+//                 });
+//             }, 1000);
+//     };
+
+//     componentDidUpdate() {
+
+//     };
+
+//     componentWillUnmount() {
+//         clearInterval(this.timerId);
+//     };
+
+//     render(){
+//         return (
+//             <p>It is {this.state.date.toLocaleTimeString()}</p>
+//         );
+//     };
+// };
+
+// export default Clock;
